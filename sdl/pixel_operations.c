57c01238f79c31b88b953a76c162ea9cb966fa0f
Erwan Dumortier <erwan.dumortier@epita.fr>
2015-10-21 15:19:42 +0200

    Correction image integrale = maintenant full fonctionelle

--- sdl/pixel_operations.c
+++ sdl/pixel_operations.c
@@ -6,7 +6,7 @@ SDL_Surface *matrix_to_img(SDL_Surface *img, Uint32 *mat)
 	Uint32 height = img->h;
 	for(Uint32 i = 1; i<height+1; i++)
 		for(Uint32 j=1; j<width+1;j++)
-			putpixel(img, i, j, mat[i*width+j]);//check
+			putpixel(img, j-1, i-1, mat[i*width+j]);
 	return img;
 }
 
@@ -20,7 +20,7 @@ Uint32 *integral_image_matrix(SDL_Surface *img)
 	Uint32 *mat = malloc((height+1)*(width+1)*sizeof(Uint32));
 	for(Uint32 i = 1; i<height+1; i++){
 		for(Uint32 j=1; j<width+1; j++){
-			mat[i*width + j] = getpixel(img, j, i) + mat[i*width+j-1] + mat[(i-1)*width + j] - mat[(i-1)*width + j-1];
+			mat[i*width + j] = getpixel(img, j-1, i-1) + mat[i*width+j-1] + mat[(i-1)*width + j] - mat[(i-1)*width + j-1];
 		}
 	}
 	return mat;
