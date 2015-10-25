ea6e1e82985eac134ab892674bc6bb7fff418a93
Guillaume Esposito <esposi_g@epita.fr>
2015-10-25 12:45:43 +0100

    Définition de la fonction + .h

---
+++ decision_stump.c
@@ -0,0 +1,12 @@
+#include "decision_stump.h"
+
+/* Pour la fonction d'appel : 
+ * int threshold = ... ; 
+ * int toggle = ... ; 
+ * int error = ... ; 
+ * int margin = ... ; 
+ * decision_stump(list_feature, &threshold, &toggle, &error, &margin */
+
+void decision_stump(t_list *list_feature, int *threshold, int *toggle, int *error, int *margin){
+
+}
---
+++ decision_stump.h
@@ -0,0 +1,8 @@
+#ifndef _DECISION_STUMP_H_
+#define _DECISION_STUMP_H_
+
+#include "list.h"
+
+void decision_stump(t_list *list_feature, int *threshold, int *toggle, int *error, int *margin);
+
+#endif
