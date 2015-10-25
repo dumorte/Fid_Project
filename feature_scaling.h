934336fb2f85c3f03e55a975037f861cff55efc8
Guillaume Esposito <esposi_g@epita.fr>
2015-10-25 16:20:37 +0100

    Modif des erreurs sur les noms des structures

--- feature_scaling.h
+++ feature_scaling.h
@@ -3,6 +3,6 @@
 
 #include "haar_feature.h"
 
-Uint32 feature_scaling(SDL_Surface *img, struct Feature *f); 
+Uint32 feature_scaling(SDL_Surface *img, t_feature *f); 
 
 #endif
--- haar_feature.h
+++ haar_feature.h
@@ -22,7 +22,7 @@ struct s_vector {
 	t_feature tab[NB_FEATURES]; 
 };
 
-struct t_vector *feature_vect(SDL_Surface *img, int x, int y); 
-struct t_vector **haar_feature(SDL_Surface *img); 
+t_vector *feature_vect(SDL_Surface *img, int x, int y); 
+t_vector **haar_feature(SDL_Surface *img); 
 
 #endif
