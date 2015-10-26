34fb731bd87859e0c54c418deaa8e0dffe46d5e8
Guillaume Esposito <esposi_g@epita.fr>
2015-10-26 09:37:15 +0100

    Début de la fonction

--- decision_stump.c
+++ decision_stump.c
@@ -7,6 +7,13 @@
  * int margin = ... ; 
  * decision_stump(list_feature, &threshold, &toggle, &error, &margin */
 
+/* "n training examples arranged in ascending order of feature pi_fx_i..." = on prend n images et pour chaque image on prend la feature f dans le vecteur des features. */
+
 void decision_stump(t_list *list_feature, int *threshold, int *toggle, int *error, int *margin){
+	/* *threshold = min(...) - 1;*/
+
+	*margin = 0; 
+	*error = 2; 
+
 
 }
