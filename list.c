cc1f5bb738b5d1807fb6f52fa907fe3f0384f25b
Guillaume Esposito <esposi_g@epita.fr>
2015-10-26 09:37:57 +0100

    Ajout de la structure t_list_surface + fonctions

--- list.c
+++ list.c
@@ -1,24 +1,26 @@
 #include "list.h"
 
-t_list *empty(){
+/* Fonctions pour t_list_feature */
+
+t_list_feature *empty_list_feature(){
 	return NULL; 
 }
 
-int is_empty(t_list *l){
+int is_empty_feature(t_list_feature *l){
 	return !l; 
 }
 
-t_list *add(t_feature *x, t_list l){
-	t_list *t = NULL; 
-	t = malloc(LSIZE); 
-	t->elem = x; 
+t_list_feature *add_feature(t_feature *x, t_list_feature *l){
+	t_list_feature *t = NULL; 
+	t = malloc(FEATURE_SIZE); 
+	t->feature = x; 
 	t->next = l; 
 	
 	return l; 
 }
 
-void free_list(t_list *l){
-	t_list *tmp; 
+void free_list_feature(t_list_feature *l){
+	t_list_feature *tmp; 
 	
 	while(l){
 		tmp = l->next; 
@@ -26,3 +28,32 @@ void free_list(t_list *l){
 		l = tmp; 
 	}
 }
+
+/* Fonctions pour t_list_surface 
+
+t_list_surface *empty_list_surface(){
+	return NULL; 
+}
+
+int is_empty_surface(t_list_surface *l){
+	return !l; 
+}
+
+t_list_surface *add_surface(SDL_Surface *x, t_list_surface *l){
+	t_list_surface *t = NULL; 
+	t = malloc(SURFACE_SIZE); 
+	t->surface = x; 
+	t->next = l; 
+
+	return l; 
+}
+
+void free_list_surface(t_list_surface *l){
+	t_list_surface *tmp; 
+
+	while(l){
+		tmp = l->next; 
+		free(l); 
+		l = tmp; 
+	}
+} */
--- list.h
+++ list.h
@@ -1,19 +1,37 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+#include <SDL/SDL.h>
+
 #include "haar_feature.h"
 
-typedef struct s_list t_list; 
-struct s_list {
-	t_feature *elem; 
-	t_list *next; 
+/*
+typedef struct s_lit_surface t_list_surface; 
+struct s_list_surface {
+	SDL_Surface *surface; 
+	t_list_surface *next; 
+};
+*/
+
+typedef struct s_list_feature t_list_feature; 
+struct s_list_feature {
+	t_feature *feature; 
+	t_list_feature *next; 
 };
 
-#define LSIZE sizeof(struct s_list)
+#define FEATURE_SIZE sizeof(struct s_list_feature)
+/* #define SURFACE_SIZE sizeof(struct s_list_surface) */
 
-t_list *empty();
-int is_empty(t_list *l); 
-t_list *add(t_feature *x, t_list l); 
-void free_list(t_list *l); 
+t_list_feature *empty_list_feature();
+int is_empty_feature(t_list_feature *l); 
+t_list_feature *add_feature(t_feature *x, t_list_feature *l); 
+void free_list_feature(t_list_feature *l); 
+
+/*
+t_list_surface *empty_list_surface(); 
+int is_empty_surface(t_list_surface *l); 
+t_list_surface *add_surface(SDL_Surface *x, t_list_surface *l); 
+void free_list_surface(t_list_surface *l); 
+*/
 
 #endif
