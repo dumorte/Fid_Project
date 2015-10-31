7e3f1b9f437c84d78738e73b44b4c949041dfa0b
Erwan Dumortier <erwan.dumortier@epita.fr>
2015-10-31 18:09:30 +0100

    suppr decision stump

--- decision_stump.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DECISION_STUMP_H_
-#define _DECISION_STUMP_H_
-
-#include "list.h"
-
-void decision_stump(t_list *list_feature, int *threshold, int *toggle, int *error, int *margin);
-
-#endif
