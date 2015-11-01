7283e66ddb4a292a5d6dd58121204645dd8c2589
Guillaume Esposito <esposi_g@epita.fr>
2015-11-02 00:34:58 +0100

    Regroupe toutes les constantes

---
+++ constantes.h
@@ -0,0 +1,9 @@
+#ifndef _CONSTANTES_H_
+#define _CONSTANTES_H_
+
+#define NB_FEATURES        162336
+#define SIZE               24
+#define PICT_WITH_FACE     11838
+#define PICT_WITH_NO_FACE  45356
+
+#endif
