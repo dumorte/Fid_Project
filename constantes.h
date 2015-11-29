5c00ff47303cb15c157182a0c2861ee809ca8aff
Guillaume Esposito <esposi_g@epita.fr>
2015-11-29 12:37:46 +0100

    Réduction des constantes

--- constantes.h
+++ constantes.h
@@ -3,7 +3,7 @@
 
 #define NB_FEATURES        162336
 #define SIZE               24
-#define PICT_WITH_FACE     11838
-#define PICT_WITH_NO_FACE  45356
+#define PICT_WITH_FACE     500 //11838
+#define PICT_WITH_NO_FACE  500 //45356
 
 #endif
