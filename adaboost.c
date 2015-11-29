26f8475a5e29fdcda14a6181d164eaa50696cbc8
Guillaume Esposito <esposi_g@epita.fr>
2015-11-29 12:38:33 +0100

    Ajout des prototypes

---
+++ adaboost.c
@@ -0,0 +1,7 @@
+void print_classifier(t_dec_stump *dec, FILE *f){
+
+}
+
+void adaboost(SDL_Surface **img_set){
+
+}
---
+++ adaboost.h
@@ -0,0 +1,11 @@
+#ifndef _ADABOOST_H_
+#define _ADABOOST_H_
+
+#include <stdlib.h>
+
+#include "classifier.h"
+
+void print_classifier(t_dec_stump *dec, FILE *f); 
+void adaboost(SDL_Surface **img_set);
+
+#endif
