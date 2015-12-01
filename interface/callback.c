fa31c05706266f385193171cab756c665a476aa8
Hussem Ben belgacem <hussem.benbelgacem@epita.fr>
2015-12-01 15:55:27 +0100

    Erreur a l'ouverture d'un fichier

--- interface/callback.c
+++ interface/callback.c
@@ -1,5 +1,29 @@
 #include "callback.h"
 
+static void open_file (const gchar *, GtkTextView *);
+
+void cb_open (GtkWidget *p_widget, gpointer user_data)
+{
+  GtkWidget *p_dialog = NULL;
+  p_dialog = gtk_file_chooser_dialog_new ("Ajout dans la base de donnee", NULL,
+                                          GTK_FILE_CHOOSER_ACTION_OPEN,
+                                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                          GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+                                          NULL);
+  if (gtk_dialog_run (GTK_DIALOG (p_dialog)) == GTK_RESPONSE_ACCEPT)
+  {
+    gchar *file_name = NULL;
+
+    file_name = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (p_dialog));
+    open_file (file_name, GTK_TEXT_VIEW (user_data));
+    g_free (file_name), file_name = NULL;
+  }
+  gtk_widget_destroy (p_dialog);
+
+  /* Parametre inutilise */
+  (void)p_widget;
+}
+
 void cb_quit (GtkWidget *p_widget, gpointer user_data)
 {
   gtk_main_quit();
@@ -7,4 +31,24 @@ void cb_quit (GtkWidget *p_widget, gpointer user_data)
   /* Parametres inutilises */
   (void)p_widget;
   (void)user_data;
+}
+
+static void open_file (const gchar *file_name, GtkTextView *p_text_view)
+{
+  g_return_if_fail (file_name && p_text_view);
+  {
+    gchar *contents = NULL;
+
+      /* Copie de contents dans le GtkTextView */
+      gchar *utf8 = NULL;
+      GtkTextIter iter;
+      GtkTextBuffer *p_text_buffer = NULL;
+
+      p_text_buffer = gtk_text_view_get_buffer (p_text_view);
+      gtk_text_buffer_get_iter_at_line (p_text_buffer, &iter, 0);
+      utf8 = g_locale_to_utf8 (contents, -1, NULL, NULL, NULL);
+      g_free (contents), contents = NULL;
+      gtk_text_buffer_insert (p_text_buffer, &iter, utf8, -1);
+      g_free (utf8), utf8 = NULL;
+    }
 }
